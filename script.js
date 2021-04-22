50d605113085effdd026a936b2ad8275b239be96
Sara Sedaghat <75505464+Sarasedaghat8960@users.noreply.github.com>
2021-04-22 21:43:55 +0200

    change of access mangodb atlas

--- script.js
+++ script.js
@@ -1,61 +1,75 @@
-fetch("http://localhost:3005/users")
+fetch("https://sarasedaghat.herokuapp.com/users")
 .then(res=>res.json())
 .then(users=>{
     let userName=document.getElementById("userName");
     let password=document.getElementById("password");
-    //localStorage.setItem("UsersInfo", JSON.stringify(users));
+//LOGIN USER
     document.getElementById("login").addEventListener("click",function(){
-        console.log(users);
-        for(user in users){
-             if(userName.value==users[user].userName && password.value==users[user].password){
-                    localStorage.setItem("UserId", JSON.stringify(users[user]._id));
-                    document.getElementById("welcomeLogin").innerHTML="";
-                    document.getElementById("welcomeLogin").insertAdjacentHTML("afterbegin","hello <b> "+users[user].name +" </b>to our page ");
-                    //console.log(users[user].userName);
-                    if(users[user].newsLetter==true){
-                        document.getElementById("changeSub").insertAdjacentHTML("afterbegin",`<br>You asked to recieve our newsletter <br><br> Do you want to change the subscription status?<br><div><button type="submit" id="cancleSubcription">Yes, I want to cancel it </button>  <button type="submit" id="Subcription">No, I do not want to cancel it </button></div>
-                        `);
-                        document.getElementById("cancleSubcription").addEventListener("click",function(){
-                            // fetch("http://localhost:3005/users")
-                            // .then(res=>res.json())
-                            // .then(users=>{
-                            // //users[user].newsLetter=false;
-                            // console.log(users[user]);});
-                            fetch("http://localhost:3005/users", {
-                                method: "POST",
-                                headers: {
-                                    "Content-Type": "application/json",
-                                },
-                                body: JSON.stringify(users[user])
-                            })
-                            .then(res=>res.json())
-                            .then(users=>{
-                                users[user].newsLetter=false;
-                                console.log(users[user]);
-                            });
+    console.log(users);
+    for(user in users){
+        if(userName.value==users[user].userName && password.value==users[user].password){
+        console.log("Current subcription",users[user].newsLetter);
+        let logInUser=users[user]
 
-                           
-                        });//function
-                        document.getElementById("Subcription").addEventListener("click",function(){
-                            users[user].newsLetter=true;
-                            console.log(users[user].newsLetter);
-                            fetch("http://localhost:3005/users", {
-                                method: "POST",
-                                headers: {
-                                    "Content-Type": "application/json",
-                                },
-                                body: JSON.stringify(users[user])
-                            })
-                            .then(res=>res.json())
-                            .then(users=> users[user].newsLetter=true);
-                        });
-                    }
-             }
+        localStorage.setItem("UserId", JSON.stringify(users[user]._id));
+        document.getElementById("welcomeLogin").innerHTML="";
+        document.getElementById("changeSub").innerHTML="";
+        document.getElementById("welcomeLogin").insertAdjacentHTML("afterbegin","hello <b> "+users[user].name +" </b>to our page ");   
+            if(users[user].newsLetter==true){
+                document.getElementById("changeSub").insertAdjacentHTML("afterbegin",users[user].name+`!You asked to recieve our newsletter <br><br> Do you want to change the subscription status?<br><div><button type="submit" id="Subcription">Yes</div>`);
+                console.log(users[user]);
+ // CANCLE SUBCRIPTION IF NEWSLETTER IF TRUE 
+                document.getElementById("Subcription").addEventListener("click",function(){
+                    console.log("user name after click yes:",logInUser.name);
+                    logInUser.newsLetter=false;
+                    let change= logInUser;
+                    
+                    console.log("user info after changed subcription  :",change);
+
+                    fetch("https://sarasedaghat.herokuapp.com/users", {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify(change)
+                    })
+                    .then(res=>res.json())
+                    .then(users=>{
+                        console.log(users);
+                    });                         
+                });
+
+            }else {
+                document.getElementById("changeSub").insertAdjacentHTML("afterbegin",`<br>You Do not like  to recieve our newsletter <br><br> Do you want to change the subscription status?<br><div><button type="submit" id="Subcription">Yes</div>`);
+ // CANCLE SUBCRIPTION IF NEWSLETTER IF False 
+                document.getElementById("Subcription").addEventListener("click",function(){
+                    console.log("user name after click yes:",logInUser.name);
+                    logInUser.newsLetter=true;
+                    let change= logInUser;
+                    
+                    console.log("user info after changed subcription  :",change);
+
+
+                    fetch("https://sarasedaghat.herokuapp.com/users", {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify(change)
+                    })
+                    .then(res=>res.json())
+                    .then(users=>{
+                        console.log(users);
+                    });                         
+                });
+
+            }
         }
-   });
-});
-// registration page 
-fetch("http://localhost:3005/users")
+    }
+   });//LOGIN
+});//THEN
+// REGISTRATION PAGE 
+fetch("https://sarasedaghat.herokuapp.com/users")
 .then(res=>res.json())
 .then(users=>console.log(users));
 
@@ -63,7 +77,7 @@ document.getElementById("register").addEventListener("click",function(){
     let newUser= {"name":document.getElementById("regName").value,"userName":document.getElementById("regUserName").value,"password":document.getElementById("regPassword").value,"newsLetter":document.getElementById("newsLetter").checked};
     console.log(newUser);
     //console.log(newUser);      
-    fetch("http://localhost:3005/users/new", {
+    fetch("https://sarasedaghat.herokuapp.com/users/new", {
     method: "POST",
     headers: {
         "Content-Type": "application/json",
